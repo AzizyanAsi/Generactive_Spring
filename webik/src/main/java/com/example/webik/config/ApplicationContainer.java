9f9a7399a288de431d02386592e21a97066356c2
unknown <azizyanasi@gmail.com>
2021-09-24 01:12:10 +0400

    updated

---
+++ webik/src/main/java/com/example/webik/config/ApplicationContainer.java
@@ -0,0 +1,10 @@
+package com.example.webik.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ApplicationContainer {
+
+    public static final ApplicationContext context =
+            new ClassPathXmlApplicationContext("applicationContext.xml");
+}
